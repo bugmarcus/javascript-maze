265a96a7bc604f61e3781a2f08ccd8097d8853fa
Marcus Queiroz <marcusq@hotmail.com>
2023-07-18 14:02:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const modeloLabirinto = [
+  "WWWWWWWWWWWWWWWWWWWWW", // linha 0
+  "W   W     W     W W W", // linha 1
+  "W W W WWW WWWWW W W W", // linha 2
+  "W W W   W     W W   W", // linha 3
+  "W WWWWWWW W WWW W W W", // linha 4
+  "W         W     W W W", // linha 5
+  "W WWW WWWWW WWWWW W W", // linha 6
+  "W W   W   W W     W W", // linha 7
+  "W WWWWW W W W WWW W F", // linha 8
+  "S     W W W W W W WWW", // linha 9
+  "WWWWW W W W W W W W W", // linha 10
+  "W     W W W   W W W W", // linha 11
+  "W WWWWWWW WWWWW W W W", // linha 12
+  "W       W       W   W", // linha 13
+  "WWWWWWWWWWWWWWWWWWWWW", // linha 14
+];
