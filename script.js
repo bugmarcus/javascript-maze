d561ab128cf10c4d7120770da9cd0bfdac33edb3
Marcus Queiroz <marcusq@hotmail.com>
2023-07-18 16:16:28 -0300

    implemented victory condition

--- script.js
+++ script.js
@@ -17,8 +17,10 @@ const modeloLabirinto = [
 ];
 let posicaoJogador = { linha: 9, coluna: 0 };
 
-function criarLabirinto(labirinto) {
+function criarLabirinto(labirinto, posicao) {
+  // Div final que receberá todas as linhas
   const divLabirinto = document.querySelector("#labirinto");
+  divLabirinto.innerHTML = "";
 
   for (let linha = 0; linha < labirinto.length; linha++) {
     const divLinha = document.createElement("div");
@@ -27,45 +29,87 @@ function criarLabirinto(labirinto) {
     for (let coluna = 0; coluna < labirinto[linha].length; coluna++) {
       const celula = document.createElement("div");
       celula.classList.add("celula");
-      //   console.log(celula);
+
       const celulaAtual = labirinto[linha][coluna];
 
+      // 1 - Quando a celula é W
       if (celulaAtual === "W") {
         celula.innerText = "W";
         celula.classList.add("parede");
-      } else if (celulaAtual === "S") {
+        // } else if (celulaAtual === "S") {
+      } else if (linha === posicao.linha && coluna === posicao.coluna) {
         celula.innerText = "S";
+        // 2 - Quando a celula é "S"
         celula.classList.add("jogador");
       } else if (celulaAtual === "F") {
         celula.innerText = "F";
+        // 3 - Quando a celula é "F"
         celula.classList.add("chegada");
-      } else {
+      } else if (celulaAtual === "") {
+        // 4 - Quando a celula é ""
         celula.classList.add("caminho");
       }
 
       divLinha.appendChild(celula);
     }
     divLabirinto.appendChild(divLinha);
+    // console.log(divLinha);
   }
-  //   console.log(divLabirinto);
+}
+
+function validaMovimento(posicao) {
+  const totalLinhas = modeloLabirinto.length; // 15
+  const totalColunas = modeloLabirinto[0].length; // 21
+  const linha = posicao.linha;
+  const coluna = posicao.coluna;
+
+  if (
+    linha < 0 ||
+    linha >= totalLinhas ||
+    coluna < 0 ||
+    coluna >= totalColunas ||
+    modeloLabirinto[linha][coluna] === "W"
+  ) {
+    return false;
+  }
+
+  // if (modeloLabirinto[linha][coluna] === "W") {
+  //   return false;
+  // }
+
+  return true;
+}
+
+function verificaVitoria(posicao) {
+  return modeloLabirinto[posicao.linha][posicao.coluna] === "F";
 }
 
 document.addEventListener("keydown", function (evento) {
   const teclaPressionada = evento.key;
-  //   console.log(evento.key);
+  const novaPosicao = {
+    linha: posicaoJogador.linha,
+    coluna: posicaoJogador.coluna,
+  };
 
-  if (teclaPressionada === "W") {
-    console.log("Tecla para cima apertada");
+  // Identificando a tecla apertada;
+  if (teclaPressionada === "ArrowUp") {
+    novaPosicao.linha -= 1;
+  } else if (teclaPressionada === "ArrowDown") {
+    novaPosicao.linha += 1;
+  } else if (teclaPressionada === "ArrowLeft") {
+    novaPosicao.coluna -= 1;
+  } else if (teclaPressionada === "ArrowRight") {
+    novaPosicao.coluna += 1;
   }
-  if (teclaPressionada === "A") {
-    console.log("Tecla para Esquerda apertada");
-  }
-  if (teclaPressionada === "S") {
-    console.log("Tecla para baixo apertada");
-  }
-  if (teclaPressionada === "D") {
-    console.log("Tecla para Direita apertada");
+
+  if (validaMovimento(novaPosicao)) {
+    posicaoJogador = novaPosicao;
+    criarLabirinto(modeloLabirinto, posicaoJogador);
+
+    if (verificaVitoria(posicaoJogador)) {
+      alert("Parabéns, você venceu!");
+    }
   }
 });
 
-criarLabirinto(modeloLabirinto);
+criarLabirinto(modeloLabirinto, posicaoJogador);
